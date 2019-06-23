42722727f2552ce5bd9853fa2749943c28c5ac1c
prsolans <prsolans@gmail.com>
2019-06-23 15:37:17 -0500

    Cleanup

--- application.py
+++ application.py
@@ -1,16 +1,8 @@
-# __init__.py
+# application.py
 from flask import Flask
 import os
 import logging
-import sqlite3
-import slack
-import ssl as ssl_lib
-import certifi
 
-from sqlite3 import Error
-from bs4 import BeautifulSoup
-from utilities.utility_scrape import simple_get
-from utilities.house_listing import HouseListing
 from utilities.house_search import get_listing_data
 
 app = Flask(__name__)
@@ -18,6 +10,6 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     listings = get_listing_data()
-    return listings
+    return listings[0]
 
 
--- utilities/house_search.py
+++ utilities/house_search.py
@@ -1,102 +1,90 @@
+# house_search.py
 import slack
 import os
 from bs4 import BeautifulSoup
 from utilities.utility_scrape import simple_get
 
 def get_listing_data():
-
-    try:
-        client = slack.WebClient(token=os.environ['SLACK_BOT_TOKEN'])
-        """## Enter the URL we are looking to crawl
-        """
-        raw_html = simple_get('https://chicago.craigslist.org/search/apa?hasPic=1&postedToday=1&bundleDuplicates=1&search_distance=6&postal=60601&min_price=2750&max_price=5000&min_bedrooms=2&min_bathrooms=2&availabilityMode=0&pets_dog=1&housing_type=4&housing_type=6&housing_type=9&laundry=1&parking=1&parking=2&parking=3&parking=4&sale_date=all+dates')
-        html = BeautifulSoup(raw_html, 'html.parser')
-
+  try:
+      client = slack.WebClient(token=os.environ['SLACK_BOT_TOKEN'])
+      """## Enter the URL we are looking to crawl
+      """
+      raw_html = simple_get('https://chicago.craigslist.org/search/apa?hasPic=1&postedToday=1&bundleDuplicates=1&search_distance=6&postal=60601&min_price=2750&max_price=5000&min_bedrooms=2&min_bathrooms=2&availabilityMode=0&pets_dog=1&housing_type=4&housing_type=6&housing_type=9&laundry=1&parking=1&parking=2&parking=3&parking=4&sale_date=all+dates')
+      html = BeautifulSoup(raw_html, 'html.parser')
 
 
-        listings = set(html.find('ul', {'class':'rows'}).findAll('li', {'class': 'result-row'}))
-        duplicateListings = set(html.find('ul', {'class':'rows'}).findAll('li', {'class': 'duplicate-row'}))
-        listings = listings - duplicateListings
 
-        listingsCount = len(listings)
+      listings = set(html.find('ul', {'class':'rows'}).findAll('li', {'class': 'result-row'}))
+      duplicateListings = set(html.find('ul', {'class':'rows'}).findAll('li', {'class': 'duplicate-row'}))
+      listings = listings - duplicateListings
 
-        # print(type(listings))
-        # print('Count: ')
-        # print(listingsCount)
+      listingsCount = len(listings)
 
-        listingTitles = []
-        listingPrices = []
+      # print(type(listings))
+      # print('Count: ')
+      # print(listingsCount)
 
-        for item in listings:
-          title = item.find('a', {'class': 'result-title'}).text
-          price = item.find('span', {'class':'result-price'}).text
-          hood = item.find('span', {'class':'result-hood'}).text
-          link = item.find('a', {'class': 'result-title'})['href']
-          about = item.find('span', {'class': 'housing'}).text
-          about = about.replace(' ', '')
-          about = about.replace('-', '')
-          
-          text = ( 
-                title + '\n' +
-                link + '\n' +                
-                price + ' |' +
-                hood + 
-                about
-                )                
+      listingTitles = []
+      listingPrices = []
+      listingText = []
 
-          print(text)
+      for item in listings:
+        title = item.find('a', {'class': 'result-title'}).text
+        price = item.find('span', {'class':'result-price'}).text
+        hood = item.find('span', {'class':'result-hood'}).text
+        link = item.find('a', {'class': 'result-title'})['href']
+        about = item.find('span', {'class': 'housing'}).text
+        about = about.replace(' ', '')
+        about = about.replace('-', '')
+        
+        text = ( 
+              title + '\n' +
+              link + '\n' +                
+              price + ' |' +
+              hood + 
+              about
+              )                
 
-          TEXT_BLOCK = {
-            'type': 'section',
-            'text': {
-              'type': 'mrkdwn',
-              'text': text
-            }
+        listingText.append(text)
+
+        TEXT_BLOCK = {
+          'type': 'section',
+          'text': {
+            'type': 'mrkdwn',
+            'text': text
           }
+        }
 
-          craft_message(client, TEXT_BLOCK)
+        # craft_message(client, TEXT_BLOCK)
 
-        
-        listingDetails = []
+      print(listingText)
+      
+      listingDetails = []
 
-        for i in range(0, listingsCount): 
-          break
-          # print('TITLE: ' + listingTitles[i].text)
-          # print('PRICE: ' + listingPrices[i].text)
+      for i in range(0, listingsCount): 
+        break
+        # print('TITLE: ' + listingTitles[i].text)
+        # print('PRICE: ' + listingPrices[i].text)
 
-        message = 'Count: ' + str(listingsCount)
-        
-        return message
+      message = 'Count: ' + str(listingsCount)
+      
+      return listingText
 
-    except Error as e:
-        print(e)
+  except Error as e:
+      print(e)
 
 def craft_message(client, TEXT_BLOCK):
-    # WELCOME_BLOCK = {
-    #   'type': 'section',
-    #   'text': {
-    #     'type': 'mrkdwn',
-    #     'text': '*A new listing has been added*'
-    #   }
-    # }
 
-    # DIVIDER_BLOCK = {'type': 'divider'}
-
+  try:
 
     text = '['+str(TEXT_BLOCK)+']'
-
     print(text)
-
 
     response = client.chat_postMessage(
             channel='#housing',
             icon_emoji=":house_buildings:",
             blocks= text
             )
+  except Error as e:
+    print(e)
 
-# if __name__ == "__main__":
-#     text = 'THIS NEW PLACE IS DOPE'
-#     craft_message(client, text)
-
-get_listing_data()
- 
